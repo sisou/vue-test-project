47247ed6405105f979946596c653305baeceb491
sisou <hello@soerenschwert.de>
2018-05-23 17:09:01 +0200

    Loop through addresses to demo DOM updates

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ Vue.config.productionTip = false
 
 const store = new Vuex.Store({
     state: {
-        msg: 'Welcome to your Vue.js App, Sören!',
+        msg: 'Welcome to your Vue.js App!',
         address: 'NQ46 6H5E CQUE B4RB J0RL FD0Q SDAV FV2T 80KG',
         accounts: {
             hasAccounts: true,
@@ -23,8 +23,8 @@ const store = new Vuex.Store({
         }
     },
     mutations: {
-        updateMsg (state, msg) {
-            state.msg = msg
+        changeAddress(state, address) {
+            state.address = address
         }
     },
     strict: process.env.NODE_ENV !== 'production'
@@ -35,4 +35,20 @@ new Vue({
     store
 }).$mount('#app')
 
-setTimeout(() => store.commit('updateMsg', 'Welcome to your Vue.js App, everybody!'), 1000)
+
+// Demo code
+
+const addresses = [
+    'NQ15 6VRE BGBN 6HCM VBYD N9E4 5X63 4D3K F8DX',
+    'NQ32 GBU4 GBAX 63RK A5T4 FB0S 1QDA QT18 S11M',
+    'NQ92 0RGS 0YGB MRY8 5MHM MBL2 HSP5 7N0U Y5C6',
+    'NQ73 CPNL VJSV LJSG X7YS BT8X 7JTS FS2K FHQH',
+    'NQ36 P00L 1N6T S3QL KJY8 6FH4 5XN4 DXY0 L7C8',
+    'NQ46 6H5E CQUE B4RB J0RL FD0Q SDAV FV2T 80KG'
+]
+
+setInterval(() => {
+    const address = addresses.shift();
+    store.commit('changeAddress', address);
+    addresses.push(address);
+}, 1000)
